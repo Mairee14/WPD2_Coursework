3f6956af0ef4a887d6c596d6f17f91e9d4b8518b
Mairee14 <m.odeh@alustudent.com>
2023-11-02 18:10:08 +0400

    Working on the event crud

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
-const path = require('path')
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const methodOverride = require('method-override')
-const session = require('express-session')
-const MongoStore = require('connect-mongo');
-const connectDB = require('./config/db')
+const express = require('express');
+const path = require("path");
+const app = express();
+const dotenv = require('dotenv');
+const connectDB = require('./config/mongoDB');
 
-// Load config
-dotenv.config({ path: './config/config.env' })
 
- connectDB()
+//load config
+dotenv.config({ path: './config/config.env' });
 
- 
-// Logging
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
-  }
+//connect to database
+connectDB();
+
 //mustache engine
-  const mustache = require('mustache-express');
-  app.engine("mustache", mustache ());
-  app.set("view engine", "mustache");
-  app.set("views", path.join(__dirname, "views"));
+const mustache = require("mustache-express");
+app.engine("mustache", mustache());
+app.set("view engine", "mustache");
+app.set('views', path.join(__dirname, 'views'));
+
 
   // Routes
-  app.use('/', require('./routes/index'))
-  app.use('/auth', require('./routes/auth'))
-  app.use('/stories', require('./routes/stories'))
+//   app.use('/', require('./routes/index'))
+//   app.use('/auth', require('./routes/auth'))
+//   app.use('/stories', require('./routes/stories'))
+
+//middlewares
+const public = path.join(__dirname, "public");
+app.use(express.static(public));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//user routes
+const userRoutes = require("./routes/backendroutes/userRoutes");
+const eventRoutes = require("./routes/backendroutes/eventRoutes");
+
+//Require routes
+const formRoutes = require("./routes/formRoutes");
+const eventformroutes =require("./routes/eventformroutes");
+
+
+
+//views routing
+app.use("/form", formRoutes);//routes for both login and signup
+app.use("/manager", userRoutes); //routing for the user data from the signup
+app.use("/events", eventRoutes);
+app.use("/eventPage", eventformroutes );
+
+
   
 
+//Routes
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-const PORT = process.env.PORT || 3000
+app.listen(PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
 
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+
